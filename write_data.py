d82ba2e88ca9509e5e605c8d0f1118b174ff8a01
I574453 <tristan.sievertsen@sap.com>
2025-02-24 11:25:42 +0100

    lets do more workflow fun

--- write_data.py
+++ write_data.py
@@ -1,16 +1,22 @@
 import json
+import argparse
 
 print("Started write_data.py")
-file = "database/test.json"
-data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str, help="filename")
+parser.add_argument("--data", type=str, help="data to write")
+
+args = parser.parse_args()
+
+file = f"database/{args.file}"
+data = args.data
+
+data_json = json.loads(data)
 
 with open(file, 'r') as f:
     file_content = json.load(f)
 
-file_content.append(data)
+file_content.append(data_json)
 with open(file, 'w') as f:
     json.dump(file_content, f)
